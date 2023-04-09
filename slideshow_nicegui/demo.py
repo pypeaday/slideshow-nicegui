bbb59db2d600cd15375ea42f9b9d12d5bec73251
pypeaday <github@pype.aleeas.com>
2023-04-09 14:11:27 -0500

    add demo

---
+++ slideshow_nicegui/demo.py
@@ -0,0 +1,16 @@
+from nicegui import ui
+
+
+class Demo:
+    def __init__(self):
+        self.number = 1
+
+
+demo = Demo()
+v = ui.checkbox("visible", value=True)
+with ui.column().bind_visibility_from(v, "value"):
+    ui.slider(min=1, max=3).bind_value(demo, "number")
+    ui.toggle({1: "A", 2: "B", 3: "C"}).bind_value(demo, "number")
+    ui.number().bind_value(demo, "number")
+
+ui.run()
